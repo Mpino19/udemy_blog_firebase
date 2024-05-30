09c3bb63b0647e9257c70bfc7c8dc939b9858b6b
Mpino19 <mykmm19@gmail.com>
2024-05-30 17:09:35 +0900

    Firestoreに投稿のデータを格納できるように実装

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = ({ isAuth }) => {
         <FontAwesomeIcon icon={faHouse} />
         ホーム
       </Link>
-      <Link to="/creatpost">
+      <Link to="/createpost">
         <FontAwesomeIcon icon={faFilePen} />
         記事投稿
       </Link>
