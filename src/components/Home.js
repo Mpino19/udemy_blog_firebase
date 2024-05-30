dc71370c5b5888a7b56be440f15e50dc0649bad0
Mpino19 <mykmm19@gmail.com>
2024-05-30 17:35:05 +0900

    Homeコンポーネントの雛型を作成

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,23 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="homepage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+        <div className="postTextContainer">
+          今はreactの勉強中です。これから勉強頑張ります。
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3>@ReactCode</h3>
+          <button>削除</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
